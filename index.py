506fb41998cf52bd77a7d4e83084cdce093761d7
Ajay Karpur <ajay@ebookglue.com>
2015-03-02 12:17:01 +0800

    added commands

--- index.py
+++ index.py
@@ -1,28 +1,28 @@
+import sys
+import getopt
+
+#-------------------------------------------------------------------------------
+
 def usage():
     print "usage: " + sys.argv[0] + " -i directory-of-documents -d dictionary-file -p postings-file"
 
-input_file_b = input_file_t = output_file = n = None
+directory = dictionary = postings = None
 
 try:
-    opts, args = getopt.getopt(sys.argv[1:], 'b:t:o:n:')
+    opts, args = getopt.getopt(sys.argv[1:], 'i:d:p:')
 except:
-    try:
-        opts, args = getopt.getopt(sys.argv[1:], 'b:t:o:')
-    except getopt.GetoptError, err:
-        usage()
-        sys.exit(2)
+    usage()
+    sys.exit(2)
         
 for o, a in opts:
-    if o == '-b':
-        input_file_b = a
-    elif o == '-t':
-        input_file_t = a
-    elif o == '-o':
-        output_file = a
-    elif o == '-n':
-        n = int(a)
+    if o == '-i':
+        directory = a
+    elif o == '-d':
+        dictionary = a
+    elif o == '-p':
+        postings = a
     else:
         assert False, "unhandled option"
-if input_file_b == None or input_file_t == None or output_file == None:
+if directory == None or dictionary == None or postings == None:
     usage()
     sys.exit(2)
---
+++ search.py
@@ -0,0 +1,30 @@
+import sys
+import getopt
+
+#-------------------------------------------------------------------------------
+
+def usage():
+    print "usage: " + sys.argv[0] + " -d dictionary-file -p postings-file -q file-of-queries -o output-file-of-results"
+
+dictionary = postings = queries = output = None
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], 'd:p:q:o:')
+except:
+    usage()
+    sys.exit(2)
+        
+for o, a in opts:
+    if o == '-d':
+        dictionary = a
+    elif o == '-p':
+        postings = a
+    elif o == '-q':
+        queries = a
+    elif o == '-o':
+        output = a
+    else:
+        assert False, "unhandled option"
+if dictionary == None or postings == None or queries == None or output == None:
+    usage()
+    sys.exit(2)
