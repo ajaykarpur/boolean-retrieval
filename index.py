6159f36bfed3591970c30f04c75eddb00c778c1c
Ajay Karpur <ajay@ebookglue.com>
2015-03-03 00:33:53 +0800

    refactored and commented index.py

--- index.py
+++ index.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 import sys
 import getopt
 import os, os.path
@@ -14,19 +15,25 @@ class Index(object):
         self.k = k
         self.dict_filename = dict_filename
         self.post_filename = post_filename
+
         self.postings = collections.defaultdict(list)
         self.dictionary = {}
+        
         self.create_postings(doc_directory)
-        pickle.dump(self.dictionary, open(self.dict_filename, "wb"))
+        self.write_files()
 
     def create_postings(self, dirname):
+        """
+        create a dict of words and their associated posting lists
+        eg. {"bill": [1, 10, 109]}
+        """
         stopwords = set(string.punctuation)
-        if "stopwords" in os.listdir(os.path.dirname(dirname)):
+        if "stopwords" in os.listdir(os.path.dirname(dirname)): # remove stopwords if given
             with open(os.path.dirname(dirname) + "\\stopwords") as s:
                 stopwords = set(s.read().split()).union(stopwords)
 
         for count, doc_id in enumerate(os.listdir(dirname)):
-            if count == self.k:
+            if count == self.k: # use k documents to train
                 break
             with open(dirname + "\\" + doc_id) as f:
                 text = f.read()
@@ -34,19 +41,29 @@ class Index(object):
                 for token in tokens:
                     word = stemmer.stem(token).lower()
                     if word not in stopwords:
-                        if self.postings[word][-1:] != [doc_id]:
+                        if self.postings[word][-1:] != [doc_id]: # check last doc_id for redundancy
                             self.postings[word].append(doc_id)
-        
-        with open(post_filename, 'w') as f:
+
+    def write_files(self):
+        """
+        write dictionary.txt and posting.txt
+        make dictionary {"word": frequency, offset}, where frequency is the size
+        of the posting list and offset is the location in postings.txt
+        """
+        with open(self.post_filename, 'w') as f: # write postings.txt
             for word in self.postings:
                 frequency = len(self.postings[word])
-                offset = f.tell()
+                offset = f.tell() # location in the postings.txt file
                 self.dictionary[word] = frequency, offset
                 
-                positions = " ".join(sorted(self.postings[word], key=int))
+                positions = " ".join(sorted(self.postings[word], key=int)) # sort postings
                 f.write(positions + "\n")
 
+        pickle.dump(self.dictionary, open(self.dict_filename, "wb")) # write dictionary.txt
+
+
 #-------------------------------------------------------------------------------
+# added a new flag to accept values of k (for subsets of k documents)
 
 def usage():
     print "usage: " + sys.argv[0] + " -i directory-of-doc_directory -d dict_filename-file -p post_filename-file"
