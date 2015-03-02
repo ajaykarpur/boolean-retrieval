826691e88dc4f3835726deb299da327331dc14fe
Ajay Karpur <ajay@ebookglue.com>
2015-03-02 15:02:25 +0800

    indexing on subset of k documents
    using -k command

--- index.py
+++ index.py
@@ -4,14 +4,18 @@ import os, os.path
 import nltk
 
 class Index(object):
-    def __init__(self, documents, dictionary, postings):
+    def __init__(self, documents, dictionary, postings, k):
+        self.k = k
         self.dictionary_filename = dictionary
         self.postings_filename = postings
         self.create_dictionary(documents)
         self.create_postings(documents)
 
     def create_dictionary(self, dirname):
-        for filename in os.listdir(dirname):
+
+        for count, filename in enumerate(os.listdir(dirname)):
+            if count == self.k:
+                break
             f = open(dirname + "\\" + filename)
             print filename
             f.close()
@@ -24,13 +28,16 @@ class Index(object):
 def usage():
     print "usage: " + sys.argv[0] + " -i directory-of-documents -d dictionary-file -p postings-file"
 
-documents = dictionary = postings = None
+documents = dictionary = postings = k = None
 
 try:
-    opts, args = getopt.getopt(sys.argv[1:], 'i:d:p:')
+    opts, args = getopt.getopt(sys.argv[1:], 'i:d:p:k:')
 except:
-    usage()
-    sys.exit(2)
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], 'i:d:p:')
+    except getopt.GetoptError, err:
+        usage()
+        sys.exit(2)
         
 for o, a in opts:
     if o == '-i':
@@ -39,10 +46,14 @@ for o, a in opts:
         dictionary = a
     elif o == '-p':
         postings = a
+    elif o == '-k':
+        k = int(a)
     else:
         assert False, "unhandled option"
 if documents == None or dictionary == None or postings == None:
     usage()
     sys.exit(2)
+if k == None:
+    k = len(os.listdir(documents))
 
-my_Index = Index(documents, dictionary, postings)
+my_Index = Index(documents, dictionary, postings, k)
