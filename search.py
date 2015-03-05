81d942efb51efe7e3cf22effab467821fffacc30
iainMeeke <meekei@tcd.ie>
2015-03-05 20:51:00 +0800

    started the function perform a query

--- search.py
+++ search.py
@@ -4,7 +4,7 @@ import getopt
 import pickle
 
 
-the_dictionary ={}
+the_dictionary = {}
 boolean_precedence = {'OR':1,'AND':2,'NOT':3,'(':0}
 opp_stack = []
 output_stack = []
@@ -18,10 +18,8 @@ def make_dict():
     """
     with open(dict_filename, 'r') as f:
         the_dictionary = pickle.load(f)
-    with open(post_filename,'r') as f:
-        f.seek(the_dictionary["bill"][1])
-        test_list = []
-        test_list = f.readline().split()
+    print the_dictionary["Gates"]
+
 # def single_query(operator,word_1,word_2 = ""):
 #     """ perform a boolean search query on word_1 and word_2 with operator. 
 #         if operator is NOT then word_2 will be empty
@@ -30,11 +28,12 @@ def make_dict():
         
 
 def make_queries():
+
     """
     goes through each line of the query file building
     a post index queue for the queries and then performing that query
     """
-    with open(queries_filename,'r') as f:
+    with open(queries_filename,'r') as f,open(post_filename,'r') as p:
         for line in f: ##go through each query
             ##opp_stack = []
            ## output_stack = []
@@ -60,12 +59,16 @@ def make_queries():
             for i in output_stack:
                print i
             # while output_stack:
-            #     token = output_stack(0)
+            #     token = output_stack.pop(0)
             #     if(token in boolean_precedence and token != 'NOT'):
             #         operand_1 = result_stack.pop()
             #         operand_2 = result_stack.pop()
-
-                    #perform_query(opperand_1,opperand_2,token)
+            #         if isinstance(operand_1, str):
+            #             operand_1 = p.seek(the_dictionary[operand_1][1])
+            #             print operand_1
+            #     else:
+            #         result_stack.append(token)
+            #         #perform_query(opperand_1,opperand_2,token)
 
 
 
@@ -129,4 +132,4 @@ if dict_filename == None or post_filename == None or queries_filename == None or
     sys.exit(2)
 
 make_dict()
-#make_queries()
+make_queries()
